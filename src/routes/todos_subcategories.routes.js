64d551916eff3acb56e3027d5e97d08972c16ccc
Romy Garcia <neitorme@gmail.com>
2023-05-31 22:00:21 -0500

    Corrigiendo ruta todos_subcategories/Id/:Id

--- src/routes/todos_subcategories.routes.js
+++ src/routes/todos_subcategories.routes.js
@@ -11,7 +11,7 @@ const router = Router();
 
 router.post("/todos_subcategories", createTodo_subcategories);
 router.get("/todos_subcategories", getTodo_subcategoriesAll);
-router.get("/todos_subcategories/:Id", getTodo_SubCategoryById);
+router.get("/todos_subcategories/Id/:Id", getTodo_SubCategoryById);
 router.get("/todos_subcategories/:subcategoriesId", getTodo_SubCategoryBySubcategoriesId);
 router.put("/todos_subcategories/:id", updateTodo_subcategories);
 router.delete("/todos_subcategories/:id", deleteTodo_subcategories);
